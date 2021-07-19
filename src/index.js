6c593f6ca737361e4f25cf771ed0770bcc3b881b
kotacky <wiss1.ide@gmail.com>
2021-07-19 17:30:38 +0900

    axiosサンプル作成

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PersonsList from './PersonsList';
+import './index.css';
+
+ReactDOM.render(
+  <PersonsList />,
+  document.getElementById('root')
+);
